4bdd1d6e3c03f0ead148e532b86e003b1808cd55
Andrew Viscu <a.viscu@student.maastrichtuniversity.nl>
2024-09-29 19:20:28 +0200

    Added echoing and threads

--- Client.java
+++ Client.java
@@ -2,12 +2,43 @@ import java.io.*;
 import java.net.*;
 
 public class Client{
-    public static void main(String[] args) throws IOException {
-        Server server = new Server();
-        final int PORT = server.getPort(); 
-        Socket socket = new Socket("localhost", PORT);
-        
-        
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.out.println("Please, specify the port.");
+            System.out.println("Usage: Client <port>");
+            System.exit(0);
+        }
+        int port = 0;
+        try {
+            port = Integer.valueOf(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Port number must be an integer.");
+            System.exit(0);
+        }
+        System.out.println("Trying to connect to localhost:" + port);
+        try {
+        Socket socket = new Socket("localhost", port); 
+
+        //Read about in Server.java
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String fromUser = stdIn.readLine();
+        String fromServer = "";
+        while(fromUser != null && fromUser.length() > 0) {
+           out.println(fromUser);
+           out.flush();
+            fromServer = in.readLine();
+            System.out.println(fromServer);
+            fromUser = stdIn.readLine();
+        }
+        in.close();
+        out.close();
         socket.close();
-    }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }       
 }
--- Server.java
+++ Server.java
@@ -1,24 +1,93 @@
 import java.io.*;
 import java.net.*;
 
-public class Server {
+public class Server extends Thread {
 
 
-    final static int PORT = 8080;
+    private int port;
+    private ServerSocket serverSocket;
+    private boolean running = false;
 
-    public int getPort() {
-        return PORT;
+    public Server(int port) {
+        this.port = port;
     }
-    public static void main(String[] args) throws IOException {
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(port);
+            this.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void stopServer() {
+        running = false;
+        this.interrupt();
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        while(running) {
+            try {
+                System.out.println("Waiting to connect...");
+                Socket socket = serverSocket.accept();
+
+                RequestHandler requestHandler = new RequestHandler(socket);
+                requestHandler.start();
+
+            } 
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
         
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Running on port " + PORT);
-        Socket clientSocket = serverSocket.accept();
+        Server server = new Server(8080);
+        System.out.println("Running on port " + server.port);
+        server.startServer();
 
-        System.out.println("Client connected!");
-        System.out.println(clientSocket.getLocalAddress());
+        try {
+            Thread.sleep(60000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        server.stopServer();
+    }
+}
 
-        clientSocket.close();
-        serverSocket.close();
+class RequestHandler extends Thread {
+    private Socket socket;
+    RequestHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Recieved a connection!");
+            //socket.getInputStream is an InputStream coming from the socket.
+            //We need to read it
+            //InputStreamReader reads character by character, so we do
+            //BufferReader and wrap it around InputStreamReader for efficiency.
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String inputLine = in.readLine();
+            while(inputLine != null && inputLine.length() > 0) {
+                out.println("Echo: " + inputLine);
+                out.flush();
+                System.out.println(inputLine + " said from " + socket.getLocalPort());
+                inputLine = in.readLine();
+            }
+            in.close();
+            out.close();
+            socket.close();
+        } 
+        catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 }
